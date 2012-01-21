dd4af811e055615e372be4b6c8dd0132b37e8181
Aditya Vaidya <kroq.gar78@gmail.com>
2012-01-21 15:13:40 -0600

    Renamed all of the package names to fit with Pizza Run.

---
+++ src/pizzarun/PizzaRunGame.java
@@ -0,0 +1,50 @@
+package pizzarun;
+
+import java.io.File;
+
+import org.lwjgl.LWJGLUtil;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class PizzaRunGame extends StateBasedGame
+{
+	public PizzaRunGame( String title )
+	{
+		super( title );
+	}
+	public PizzaRunGame()
+	{
+		this("Tilt");
+	}
+	
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException
+	{
+		this.addState(new GameplayState());
+	}
+	
+	public static void main( String[] args ) throws SlickException
+	{
+		System.setProperty("org.lwjgl.librarypath", new File(new File(System.getProperty("user.dir"), "native"), LWJGLUtil.getPlatformName()).getAbsolutePath());
+		System.setProperty("net.java.games.input.librarypath", System.getProperty("org.lwjgl.librarypath"));
+		
+		AppGameContainer app = new AppGameContainer( new PizzaRunGame() );
+		
+		app.setDisplayMode( WIDTH , HEIGHT , FULLSCREEN );
+		app.setSmoothDeltas(SMOOTH_DELTAS);
+		app.setShowFPS(SHOW_FPS);
+		app.setTargetFrameRate(MAX_FPS);
+		
+		app.start();
+	}
+	
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = 480;
+	public static final boolean FULLSCREEN = false;
+	public static final boolean SMOOTH_DELTAS = true;
+	public static final boolean SHOW_FPS = true;
+	public static final int MAX_FPS = 60;
+}
